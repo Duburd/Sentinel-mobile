85edcf091c5291e85af62bbeba08b0995f5ec2a1
Mattison Greff <greff.matt@icloud.com>
2018-07-05 19:05:55 -0700

    add touch events for going through tooltips, add request for user and vehicle data

--- src/roadside_assistance/start_report/Form.js
+++ src/roadside_assistance/start_report/Form.js
@@ -1,39 +1,60 @@
 import React from 'react';
-import { Picker, Animated, Alert, AppRegistry, Button, StyleSheet, View, Text } from 'react-native';
+import { Picker, Animated, Alert, AppRegistry, Button, StyleSheet, ScrollView, Text } from 'react-native';
 import ProgressBar from './ProgressBar'
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import fake_user from './fake_user.json';
 
 export default class Form extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      currentDriver: 'Matt Greff',
-      currentVehicle: 'BMW M3',
+      currentDriver: '',
+      currentVehicle: '',
+
     }
   }
+
+  componentDidMount(){
+    fetch('https://alluring-shenandoah-49358.herokuapp.com/api/users/3')
+      .then((results)=> results.json())
+      .then((users_data) => {
+        users = users_data.map( (_) => (
+          <Picker.Item label= {`${_.first_name} ${_.last_name}`} value={_.id}/>
+        ))
+        this.setState({users})});
+    fetch('https://alluring-shenandoah-49358.herokuapp.com/api/users/10/vehicles')
+      .then((results) => results.json())
+      .then((vehicle_data) => {
+        vehicles = vehicle_data.map((_) => (
+            <Picker.Item label= {`${_.make} ${_.model}`} value={_.id}/>
+      ))
+        this.setState({vehicles})
+      });
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View height={'100%'} width={'100%'} style={{flex: 1}}>
+      <ScrollView height={'100%'} width={'100%'} style={{flex: 1}}>
 
         <Text style={styles.title}>Report</Text>
+        <FormLabel>Current Driver</FormLabel>
         <Picker 
           selectedValue = {this.state.currentDriver}
-          style={{ height: 50, width: '80%'}}
+          style={{ height: 50, width: "100%" }}
           onValueChange={(itemValue, itemIndex) => this.setState({currentDriver: itemValue})}>
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
+          {this.state.users}
         </Picker>
+        <FormLabel>Current Vehicle</FormLabel>
         <Picker
           selectedValue={this.state.currentVehicle}
-          style={{ height: 50, width: 100 }}
+          style={{ height: 50, width: "100%" }}
           onValueChange={(itemValue, itemIndex) => this.setState({currentVehicle: itemValue})}>
-          <Picker.Item label="Java" value="java" />
-          <Picker.Item label="JavaScript" value="js" />
+          {this.state.vehicles}
         </Picker>
         <Text style={styles.subtitle}>Report</Text>
         <FormLabel>Name</FormLabel>
-        <FormInput />
+        <FormInput onChangeText={(text) => this.setState({username: text})} value={this.state.username}/>
         <FormValidationMessage>Error message</FormValidationMessage>
         <FormLabel>Name</FormLabel>
         <FormInput />
@@ -48,7 +69,7 @@ export default class Form extends React.Component {
         <FormInput />
         <FormValidationMessage>Error message</FormValidationMessage>
         
-      </View>
+      </ScrollView>
     );
   }
 }
--- src/roadside_assistance/start_report/ToolTips1.js
+++ src/roadside_assistance/start_report/ToolTips1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated, Alert, AppRegistry, Button, StyleSheet, View, Text } from 'react-native';
+import { Animated, Alert, AppRegistry, TouchableOpacity, StyleSheet, View, Text } from 'react-native';
 import ProgressBar from './ProgressBar'
 import { Tile } from 'react-native-elements';
 
@@ -7,33 +7,37 @@ import { Tile } from 'react-native-elements';
 export default class ToolTips1 extends React.Component {
   constructor(props){
     super(props)
-    this.state = {}
+    this.state = {
+      tooltips: [
+        'Roadside Assistance?',
+        'As difficult as it may seem, it is important that you remain calm.',
+        'Do not argue with other drivers and passengers. Save your story for the police.',
+        'Do not voluntarily assume liability or take responsibility, sign statements regarding fault, or promise to pay for damage at the scene of the accident.',
+        'Stop. If your vehicle is involved in an accident and you don\'t stop, you may be subject to criminal prosecution.',
+        'If no one is injured and total damage to all the vehicles involved appears to be less than $2,000, call a Collision Reporting Centre',
+        'Otherwise. Call the police.',
+        'If safe to do so, let\'s take some pictures to document the situation.'
+      ],
+      index: 0,
+    }
   }
+
+  nextSlide = () => {
+    index = this.state.index + 1;
+    this.setState({
+      index
+    })
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View height={'100%'} width={'100%'}>
-        <Tile imageSrc={ require('./../../../assets/images/contact.jpg')}    
-          height={200} 
-          title={'back from Tooltips'}
-          icon={{ name: 'ios-phone-portrait', type: 'ionicon', color: 'white', size: 40}}
-          featured
-          onPress={()=> navigate('options')}
-        />
-        <Tile imageSrc={ require('./../../../assets/images/contact.jpg')}    
-          height={200} 
-          title={'form'}
-          icon={{ name: 'ios-phone-portrait', type: 'ionicon', color: 'white', size: 40}}
-          featured
-          onPress={()=> navigate('form')}
-        />
-        <Tile imageSrc={ require('./../../../assets/images/contact.jpg')}    
-          height={200} 
-          title={'camera'}
-          icon={{ name: 'ios-phone-portrait', type: 'ionicon', color: 'white', size: 40}}
-          featured
-          onPress={()=> navigate('camera')}
-        />
+      <View height={'100%'} width={'100%'} >
+        <TouchableOpacity style={{width: '100%', height: '100%'}} onPress={this.nextSlide}>
+          <Text>
+            {this.state.tooltips[this.state.index]}
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
