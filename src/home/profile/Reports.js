0ce49fc6a7a3323d12be64bfb7f1c872c9cce10d
Mattison Greff <greff.matt@icloud.com>
2018-07-09 10:51:14 -0700

    add placeholder untill report route is finished

--- src/home/profile/Reports.js
+++ src/home/profile/Reports.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Animated, Alert, AppRegistry, Button, StyleSheet, View, Text } from 'react-native';
-import { Avatar } from 'react-native-elements';
-import fake_user from './fake_user.json';
+import { Card, Icon } from 'react-native-elements';
 
 export default class Reports extends React.Component {
   constructor(props) {
@@ -11,9 +10,19 @@ export default class Reports extends React.Component {
   componentDidMount(){}
   render() {
     return (
-      <View style={styles.column}>
-        <Text style={styles.title}>REPORTS</Text>
-      </View>
+      <Card
+        title='HELLO WORLD'
+        >
+        <Text style={{marginBottom: 10}}>
+          The idea with React Native Elements is more about component structure than actual design.
+        </Text>
+        <Button
+          icon={<Icon name='code' color='#ffffff' />}
+          backgroundColor='#03A9F4'
+          fontFamily='Lato'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title='VIEW NOW' />
+      </Card>
     );
   }
 }
