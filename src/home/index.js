5b297dec67ab3b2b576edccb32ac99693aa9aa8b
Mattison Greff <greff.matt@icloud.com>
2018-07-09 22:17:02 -0700

    add clarification for user login route

--- src/home/index.js
+++ src/home/index.js
@@ -5,14 +5,17 @@ import Nav from './Nav'
 import Profile from './profile/Main'
 import Reports from './profile/Reports'
 import Vehicles from './profile/Vehicles'
-import Login from './profile/Login'
+import Login from './Login'
 
 export default class HomeIndex extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       tab: <Profile/>,
-      modalVisible: true,
+      isLoggedIn: false,
+      policyNum: '',
+      pwd: '',
+      logErr: null
     }
   }
 
@@ -21,17 +24,50 @@ export default class HomeIndex extends React.Component {
     this.setState({tab})
   }
 
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+  tryLogin = () => {
+    loginObj = {
+      policyNum: this.state.policyNum,
+      pwd: this.state.pwd,
+    }
+    cred = JSON.stringify(loginObj)
+    fetch('https://alluring-shenandoah-49358.herokuapp.com/api/users/session', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: cred,
+    })
+    .then(function (response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return response.json()
+      .catch(function (logErr) {
+        console.log(logErr)
+      });
+    })
+    this.fadeModal()
   }
 
+  fadeModal() {
+    this.setState({isLoggedIn: true });
+  }
+
+  onPolicyChange = (text) => this.setState({policyNum: text})
+  onPwdChange    = (text) => this.setState({pwd:       text})
+
   render() {
+    const { pwd, policyNum, isLoggedIn , logErr} = this.state;
     return (
       <View height={'100%'} width={'100%'}>
         <FadeInView navigation={this.props.navigation}/>
-          <Login />
+        <Login 
+          tryLogin={this.tryLogin}  
+          login={this.login}
+          fromIndex={{pwd, policyNum, isLoggedIn, logErr}}
+          onPolicyChange={this.onPolicyChange}
+          onPwdChange={this.onPwdChange}
+        />
         {this.state.tab}
-        <Nav currentTab={this.currentTab}/>
+        <Nav currentTab={this.currentTab} />
       </View>
     );
   }
