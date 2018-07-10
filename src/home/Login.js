a76c929b0e9c889fca9e40ff4cebae1b5c421163
Mattison Greff <greff.matt@icloud.com>
2018-07-10 10:21:31 -0700

    style login screen

--- src/home/Login.js
+++ src/home/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Platform, Picker, Animated, Alert, AppRegistry, StyleSheet, ScrollView, Text, Modal, View} from 'react-native';
-import { FormInput, FormLabel, Button, FormValidationMessage} from 'react-native-elements'
+import { Platform, Picker, Animated, Alert, AppRegistry, StyleSheet, ScrollView, Text, Modal, View, Image} from 'react-native';
+import { FormInput, FormLabel, Button, FormValidationMessage, Tile} from 'react-native-elements'
 
 export default class Form extends React.Component {
   constructor(props){
@@ -15,10 +15,17 @@ export default class Form extends React.Component {
       visible={!this.props.fromIndex.isLoggedIn}
       onRequestClose={() => {
         alert('You are now logged in.');
-      
       }}>
         <View style={{flex:1, justifyContent: 'center'}}>
-        <Text style={styles.title}>Login</Text>
+        <Tile 
+        imageSrc={{uri:'https://steemitimages.com/0x0/https://plotagraph.s3.amazonaws.com/shared/589e4c6ed1e4e81500537144.gif'}}    
+        height={200}
+        title={'Sentinel Login'}
+        titleStyle={{fontSize: 50}}
+        featured
+        onPress={()=> ''}
+      />
+        <Text style={styles.login}></Text>
         <FormValidationMessage labelStyle={{textAlign: 'center'}}>{this.props.fromIndex.logErr}</FormValidationMessage>
         <FormLabel>Policy Number</FormLabel>
         <FormInput 
@@ -44,6 +51,14 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   title: {
+    fontSize: 40,
+    textAlign: 'center',
+    color: 'black',
+    borderRadius: 0,
+    fontWeight: 'bold',
+    color: 'lightblue'
+  },
+  login: {
     fontSize: 40,
     textAlign: 'center',
     color: 'black',
